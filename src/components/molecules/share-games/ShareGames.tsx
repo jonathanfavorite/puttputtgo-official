06ce716f47d60af8fde8cc3cfc97362f712b9375
Jonathan Favorite <jonathanfavorite@hotmail.com>
2023-07-12 23:45:31 -0400

    sharer

--- src/components/molecules/share-games/ShareGames.tsx
+++ src/components/molecules/share-games/ShareGames.tsx
@@ -6,11 +6,25 @@ import { GameContext } from '../../../contexts/GameContext';
 
 function ShareGames() {
     const _gameContext = useContext(GameContext);
+
+    function handleMessageClick() {
+        if (navigator.share) {  
+            navigator.share({
+              title: 'Check this out!',
+              text: 'Hello, check out this cool website I found.',
+              url: `https://www.facebook.com/sharer/sharer.php?u=https://www.favoritecreative.com/puttputtgo/sharer.php?customerKey=${_gameContext.companyData.customerID}&gameID=${_gameContext.gameID}`
+            })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+          } else {
+            console.log('Share not supported on this browser, do it the old way.');
+          }
+    }
   return (
     <div className='share-game-wrap'>
         <div className='icon facebook'><a href={`https://www.facebook.com/sharer/sharer.php?u=https://www.favoritecreative.com/puttputtgo/sharer.php?customerKey=${_gameContext.companyData.customerID}&gameID=${_gameContext.gameID}`}><Icons.Facebook /></a></div>
         <div className='icon twitter'><Icons.Twitter /></div>
-        <div className='icon message'><Icons.Message /></div>
+        <div className='icon message' onClick={handleMessageClick}><Icons.Message /></div>
     </div>
   )
 }
