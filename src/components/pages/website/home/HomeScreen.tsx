bc37582d55de86017d169d7436a9527382bfcab1
Jonathan Favorite <jonathanfavorite@hotmail.com>
2023-07-17 23:07:56 -0400

    more para

--- src/components/pages/website/home/HomeScreen.tsx
+++ src/components/pages/website/home/HomeScreen.tsx
@@ -21,7 +21,8 @@ A new golf experience</h1>
       <div className='desc'>
       <p>Welcome to PuttPuttGo, where we're redefining what it means to golf in the digital age.</p>
       
-      <p>Our innovative mobile application transforms your traditional scorecard into an interactive, personalized tool that does more than just keep track of your strokes. With PuttPuttGo, you can analyze your game in-depth, uncover your best and worst holes, and share your achievements on the digital green.</p>
+      <p>Our innovative mobile application transforms your traditional scorecard into an interactive, personalized tool that does more than just keep track of your strokes.</p> 
+      <p>With PuttPuttGo, you can analyze your game in-depth, uncover your best and worst holes, and share your achievements on the digital green.</p>
       
       <p>Enjoy a seamless, modern golf experience, whether you're at a miniature golf course or teeing off on a full 18-hole course. Step beyond the green with PuttPuttGo, and embrace a new golfing experience. </p>
       </div>
