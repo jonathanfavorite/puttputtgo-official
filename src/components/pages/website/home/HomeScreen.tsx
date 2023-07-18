612714f9f97faff08f89556d389a9d912fcfe321
Jonathan Favorite <jonathanfavorite@hotmail.com>
2023-07-17 23:07:04 -0400

    paragraphs

--- src/components/pages/website/home/HomeScreen.tsx
+++ src/components/pages/website/home/HomeScreen.tsx
@@ -19,7 +19,11 @@ function HomeScreen() {
           <h1>Beyond the Green:<br />
 A new golf experience</h1>
       <div className='desc'>
-      <p>Welcome to PuttPuttGo, where we're redefining what it means to golf in the digital age. Our innovative mobile application transforms your traditional scorecard into an interactive, personalized tool that does more than just keep track of your strokes. With PuttPuttGo, you can analyze your game in-depth, uncover your best and worst holes, and share your achievements on the digital green. Enjoy a seamless, modern golf experience, whether you're at a miniature golf course or teeing off on a full 18-hole course. Step beyond the green with PuttPuttGo, and embrace a new golfing experience. </p>
+      <p>Welcome to PuttPuttGo, where we're redefining what it means to golf in the digital age.</p>
+      
+      <p>Our innovative mobile application transforms your traditional scorecard into an interactive, personalized tool that does more than just keep track of your strokes. With PuttPuttGo, you can analyze your game in-depth, uncover your best and worst holes, and share your achievements on the digital green.</p>
+      
+      <p>Enjoy a seamless, modern golf experience, whether you're at a miniature golf course or teeing off on a full 18-hole course. Step beyond the green with PuttPuttGo, and embrace a new golfing experience. </p>
       </div>
         </div>
       </div>
