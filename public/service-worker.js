d15a2b382d3834a2be0165e0ad5d1c4335569c67
Jonathan Favorite <jonathanfavorite@hotmail.com>
2023-07-18 22:17:57 -0400

    updated service worker to ignore js files

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,12 +23,33 @@ self.addEventListener('message', (event) => {
   self.addEventListener('fetch', (event) => {
     event.respondWith(
       (async () => {
+        // Get the URL of the request
+        const requestURL = new URL(event.request.url);
+  
+        // If the request is for a JavaScript file...
+        if (requestURL.pathname.endsWith('.js')) {
+          // ...try fetching from the network first.
+          try {
+            return await fetch(event.request);
+          } catch (err) {
+            // If the network request fails, try to serve from cache.
+            const cachedResponse = await caches.match(event.request);
+            if (cachedResponse) {
+              return cachedResponse;
+            }
+            // If there is no cached response, throw the original error.
+            throw err;
+          }
+        }
+  
+        // If the request is not for a JavaScript file...
         const cachedResponse = await caches.match(event.request);
         if (cachedResponse) {
-         
+          // ...return the cached response if there is one.
           return cachedResponse;
         }
-   
+  
+        // If there is no cached response, fetch from the network.
         return fetch(event.request);
       })()
     );
