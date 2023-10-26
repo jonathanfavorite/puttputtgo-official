617adc107f45d082aeafe3f66af28e29f69ea7d2
Jonathan Favorite <jonathanfavorite@hotmail.com>
2023-10-26 19:14:25 -0400

    more changes

--- functions/meta-server-function.tsx
+++ functions/meta-server-function.tsx
@@ -30,13 +30,13 @@ const customerDetails = [
 exports.handler = async function(event, context) {
   // Check the user-agent from the request headers
   const userAgent = event.headers['user-agent'].toLowerCase();
-  const isCrawler = userAgent.includes('googlebot') || userAgent.includes('facebookexternalhit'); // ... other crawlers
+  //const isCrawler = userAgent.includes('googlebot') || userAgent.includes('facebookexternalhit'); // ... other crawlers
 
 
    const pathParameters = event.path.split("/");
    const customer = pathParameters[1]; 
 
-  if (isCrawler) {
+  if (customer) {
     // Extract gameID from query parameters
     const { gameID } = event.queryStringParameters;
     const gameData = await fetchGameData(gameID);
@@ -75,31 +75,11 @@ exports.handler = async function(event, context) {
       body: htmlResponse,
     };
   } else {
-    try {
-      // Construct the file path. Since we're not sure of the exact structure, we'll use an environment variable.
-      // NETLIFY_BUILD_BASE is an environment variable provided by Netlify that gives the base path.
-      const basePath = process.env.NETLIFY_BUILD_BASE || '/var/task/';
-      const filePath = path.join(basePath, 'build', 'index.html');
-
-      // Attempt to read the file from this path
-      const indexHtml = fs.readFileSync(filePath, 'utf-8');
-
-      // Serve the file's contents
-      return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'text/html',
-        },
-        body: indexHtml,
-      };
-    } catch (error) {
-      console.error('Error serving index.html:', error);
-      // If there's an issue reading the file, log the error and return a 500 status code
-      return {
-        statusCode: 500,
-        body: 'Internal Server Error: Unable to serve page: ' + JSON.stringify(error),
-      };
-    }
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html' },
+      body: "sorry",
+    };
   }
 };
 
